7520fd7fd89d2507a3f36db7212783e6fc7b48e8
Cupid Chan <cupidchan@hotmail.com>
2021-05-03 17:48:08 -0400

    Fix test break

--- src/tests/example.ts
+++ src/tests/example.ts
@@ -1,6 +1,12 @@
+import 'reflect-metadata'
+import {} from 'jasmine';
+
 // A sample Jasmine test
 describe("A suite", function() {
   it("contains spec with an expectation", function() {
     expect(true).toBe(true);
   });
+  it("should fail", function() {
+    expect(true).toBe(false);
+  });  
 });
